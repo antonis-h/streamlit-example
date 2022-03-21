bb596100d1f8d68e0f9ae7a30d5c3f0eb762fb19
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 18:27:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+%matplotlib inline
 
 st.write("""
 ## Superstore Dataset
