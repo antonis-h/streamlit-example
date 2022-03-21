1cda556f21a24dcb34bd21662f61f95ed2cc27f6
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 18:18:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,29 @@ import pandas as pd
 import streamlit as st
 
 st.write("""
-## Checking the buttons
-
-Go ahead and experiment!
+## Superstore Dataset
 """)
 
 with st.sidebar:
     st.write('External features')
-    for i in range(5):
+    for i in range(1,5):
         box = st.checkbox(f'Option {i}')
 
-st.title('Experimenting with Streamlit')
+data = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
+data['Order Date'] = pd.to_datetime(data['Order Date'], format = '%m/%d/%Y')
+
+st.title('Daily Sales')
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+day_chosen = st.sidebar.selectbox('Choose a day', days)
+sales_day = data[data['Order Date'].dt.day_name() == f'{day_chosen}']
+
+fig = plt.plot(sales_day['Order Date'],sales_day['Sales'].rolling(300).mean(),'x')
+st.plotly_chart(fig)
 
 click = st.button("Don't press too hard")
 
 if click:
-    st.write(""""
+    st.write("""
     Ouchhh :cry:
     """)
     
