5f1258b15e1870c901b307987325ad35065f178d
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 16:55:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ with st.echo(code_location='below'):
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
     
-this = st.text_input()
+this = st.text_input('What')
 
 st.title(this)
