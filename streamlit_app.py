6469854d59ad62f94f0d30c791c1471914d34f58
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 17:32:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ import streamlit as st
 
 st.title('Experimenting with Streamlit')
 
-click = st.button("Don't press too hard, :cry:")
+click = st.button("Don't press too hard")
 
 if click:
-    st.write('Oucchhh ')
+    st.write('Oucchhh ',:cry:)
 this = st.text_input('What')
 
 st.title(this)
