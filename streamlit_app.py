11b2d33a8cd9fa518e9db170e9630c881e79055b
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 19:37:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,10 @@ days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sun
 day_chosen = st.sidebar.selectbox('Choose a day', days)
 sales_day = data[data['Order Date'].dt.day_name() == f'{day_chosen}']
 
-fig, ax = plt.subplot()
-ax.plot(sales_day['Order Date'],sales_day['Sales'].rolling(300).mean(),'x')
-plt.title(f'{day_chosen} Sales')
-st.pyplot(fig)
+# fig, ax = plt.subplot()
+# ax.plot(sales_day['Order Date'],sales_day['Sales'].rolling(300).mean(),'x')
+# plt.title(f'{day_chosen} Sales')
+# st.pyplot(fig)
 
 click = st.button("Don't press too hard")
 
