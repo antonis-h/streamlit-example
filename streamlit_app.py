b1475404dadb30ed94a64c5f288ec47d91024591
Antonis Hadjiiona <59941598+antonis-h@users.noreply.github.com>
2022-03-21 17:36:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,20 @@ import math
 import pandas as pd
 import streamlit as st
 
+st.write("""
+##Checking the buttons :cry:
+
+Go ahead and experiment!
+""")
+
 st.title('Experimenting with Streamlit')
 
 click = st.button("Don't press too hard")
 
 if click:
-    st.write('Oucchhh ' :cry:)
+    st.write('Oucchhh ')
+    
+    
 this = st.text_input('What')
 
 st.title(this)
